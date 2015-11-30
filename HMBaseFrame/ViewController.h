f027f0d7a061b2eb8edaa7b481b35a445f8ad92c
微小宝 <appstorevxbao@hotmail.com>
2015-11-30 17:34:37 +0800

    创建项目

---
+++ HMBaseFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMBaseFrame
+//
+//  Created by leo on 15/11/30.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HMBaseFrameTests/HMBaseFrameTests.m
@@ -0,0 +1,39 @@
+//
+//  HMBaseFrameTests.m
+//  HMBaseFrameTests
+//
+//  Created by leo on 15/11/30.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HMBaseFrameTests : XCTestCase
+
+@end
+
+@implementation HMBaseFrameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
