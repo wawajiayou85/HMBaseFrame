0aa08d46ae9b0f7728455f872370d1f4f34ffeab
韩铭 <hanm1985@163.com>
2015-12-21 13:56:56 +0800

    no message

---
+++ HMBaseFrame/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  HMBaseFrame
+//
+//  Created by leo on 15/12/21.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ HMBaseFrame/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  HMBaseFrame
+//
+//  Created by leo on 15/12/21.
+//  Copyright © 2015年 leo. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
